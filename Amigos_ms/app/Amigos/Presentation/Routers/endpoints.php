3411a17907b22c59b434de9bf7e694080ed9980d
Sala D17 <CSalaD17@uniboyaca.edu.co>
2026-04-25 12:31:29 -0500

    Endpoints

--- Amigos_ms/app/Amigos/Presentation/Routers/endpoints.php
+++ Amigos_ms/app/Amigos/Presentation/Routers/endpoints.php
@@ -1,10 +1,25 @@
 <?php
-
+ 
 use Slim\App;
 use App\Amigos\Presentation\Repositories\TestRepository;
+use App\Amigos\Presentation\Repositories\AmigosRepository;
 use Slim\Routing\RouteCollectorProxy;
-
+ 
 return function (App $app) {
     $app->get('/test', [TestRepository::class, 'hola']);
-    $app->post('/crearamigos', [AmigosRepository::class, 'create']);
+ 
+    $app->post('/amigo', [AmigosRepository::class, 'create']);
+    $app->get('/amigos', [AmigosRepository::class, 'all']);
+    $app->get('/amigo/{id}', [AmigosRepository::class, 'detail']);
+    $app->put('/amigo/{id}', [AmigosRepository::class, 'update']);
+    $app->delete('/amigo/{id}', [AmigosRepository::class, 'delete']);
+ 
+    $app->group('/amigos-v2', function (RouteCollectorProxy $group) {
+        $group->get('', [AmigosRepository::class, 'all']);
+        $group->get('/{id}', [AmigosRepository::class, 'detail']);
+        $group->post('', [AmigosRepository::class, 'create']);
+        $group->put('/{id}', [AmigosRepository::class, 'update']);
+        $group->delete('/{id}', [AmigosRepository::class, 'delete']);
+    });
 };
+ 
