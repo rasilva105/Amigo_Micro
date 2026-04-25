3bc821bbed8d574a243cf3121722f24a030e62cb
Sala D17 <CSalaD17@uniboyaca.edu.co>
2026-04-25 12:14:39 -0500

    AP

---
+++ Amigos_ms/app/Amigos/Models/Amigo.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Amigos\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Amigo extends Model {
+
+    protected $table = 'amigos';
+    public $timestamps = false;
+}
